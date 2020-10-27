2dfb509b44b57ad3a9736f361492ba6194a964e5
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2020-10-26 23:05:09 -0300

    feat: add create region

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 
 const UserController = require('../app/controllers/UserController');
+const RegionController = require('../app/controllers/RegionController');
 const authMiddleware = require('../app/middlewares/auth');
 
 routes.post('/users', UserController.create);
@@ -8,5 +9,7 @@ routes.post('/users/authenticate', UserController.authenticate);
 
 // Authenticated routes
 routes.use(authMiddleware);
+
+routes.post('/region', RegionController.create);
 
 module.exports = routes;
